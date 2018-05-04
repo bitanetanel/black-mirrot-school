56129e4b7828ae4e94d44fe7ad6912cb3b75d072
Netanel Bitan <bitanetanel@gmail.com>
2018-05-04 13:14:03 +0300

    Handle exceptions

--- src/black_mirror/console_app.py
+++ src/black_mirror/console_app.py
@@ -25,54 +25,11 @@ class ConsoleApp(object):
         option = input("Please select option: ")
 
         while option:
-            if option == 1:
-                self.add_user()
-            if option == 2:
-                print "Adding a teacher"
-                name = raw_input("Name: ")
-                phone = raw_input("Phone: ")
-                class_number = input("Class number: ")
-                self.school.add_teacher(name, phone, class_number)
-            if option == 3:
-                print "Enter a class"
-                student_id = input("Student id")
-                class_number = input("Class number")
-                self.school.enter_class(student_id, class_number)
-            if option == 4:
-                print "Exit a class"
-                student_id = input("Student id")
-                class_number = input("Class number")
-                self.school.exit_class(student_id, class_number)
-            if option == 5:
-                print "Eating"
-                student_id = input("Student id")
-                self.school.get_student(student_id).eat()
-            if option == 6:
-                pass
-            if option == 7:
-                print "Students:"
-                print self.school.students
-            if option == 8:
-                print "Teachers:"
-                print self.school.teachers
-            if option == 9:
-                print "Whe ate in last 60 minutes?"
-                eat_events = EventsLogger.events.get(EventTypes.EAT)
-                if not eat_events:
-                    print "No one."
-                else:
-                    eat_events.reverse()
-                    for eat_event in eat_events:
-                        delta = datetime.now() -eat_event.creation_time
-                        if delta.seconds / 60 > 60:
-                            break
-                        print eat_event.student.name
-            if option == 10:
-                print "Presence list"
-                for klass in self.school.classes:
-                    print "Presence in {}:".format(klass.class_number)
-                    print klass.student_in_class_now
-
+            try:
+                self.run_option(option)
+            except Exception, e:
+                print "Got an error: {}".format(e.message)
+                print "Please try again."
             self.print_menu()
             option = input("Please select option: ")
 
@@ -83,5 +40,58 @@ class ConsoleApp(object):
         age = raw_input("Age: ")
         student_id = self.school.add_student(name, phone, age)
         for i in xrange(DAY_HOURS):
-            class_number = input("Select class for hour {}: ".format(i))
-            self.school.get_student(student_id).set_class(class_number, i)
+            try:
+                class_number = input("Select class for hour {}: ".format(i))
+                self.school.get_student(student_id).set_class(class_number, i)
+            except Exception, e:
+                print "Got an error: {}".format(e.message)
+                print "Please try again."
+
+    def run_option(self, option):
+        if option == 1:
+            self.add_user()
+        if option == 2:
+            print "Adding a teacher"
+            name = raw_input("Name: ")
+            phone = raw_input("Phone: ")
+            class_number = input("Class number: ")
+            self.school.add_teacher(name, phone, class_number)
+        if option == 3:
+            print "Enter a class"
+            student_id = input("Student id")
+            class_number = input("Class number")
+            self.school.enter_class(student_id, class_number)
+        if option == 4:
+            print "Exit a class"
+            student_id = input("Student id")
+            class_number = input("Class number")
+            self.school.exit_class(student_id, class_number)
+        if option == 5:
+            print "Eating"
+            student_id = input("Student id")
+            self.school.get_student(student_id).eat()
+        if option == 6:
+            pass
+        if option == 7:
+            print "Students:"
+            print self.school.students
+        if option == 8:
+            print "Teachers:"
+            print self.school.teachers
+        if option == 9:
+            print "Whe ate in last 60 minutes?"
+            eat_events = EventsLogger.events.get(EventTypes.EAT)
+            if not eat_events:
+                print "No one."
+            else:
+                eat_events.reverse()
+                for eat_event in eat_events:
+                    delta = datetime.now() - eat_event.creation_time
+                    if delta.seconds / 60 > 60:
+                        break
+                    print eat_event.student.name
+        if option == 10:
+            print "Presence list"
+            for klass in self.school.classes:
+                print "Presence in {}:".format(klass.class_number)
+                print klass.student_in_class_now
